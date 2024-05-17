589ff8b907bb240aa18ac578bb996dd5e8e2d5cd
bakhrom-carma <bakhrom@joincarma.com>
2024-05-17 14:01:13 +0100

    first commit

---
+++ data/generated/bib.js
@@ -0,0 +1,102 @@
+﻿define({ entries : {
+    "bahdanau2015neural": {
+        "author": "Bahdanau, Dzmitry and Cho, Kyunghyun and Bengio, Yoshua",
+        "booktitle": "International Conference on Learning Representations",
+        "doi": "10.48550/arXiv.1409.0473",
+        "title": "Neural Machine Translation by Jointly Learning to Align and Translate",
+        "type": "inproceedings",
+        "url": "https://doi.org/10.48550/arXiv.1409.0473",
+        "year": "2015"
+    },
+    "brown2020language": {
+        "author": "Brown, Tom and others",
+        "doi": "10.48550/arXiv.2005.14165",
+        "journal": "arXiv preprint arXiv:2005.14165",
+        "title": "Language Models are Few-Shot Learners",
+        "type": "article",
+        "url": "https://doi.org/10.48550/arXiv.2005.14165",
+        "year": "2020"
+    },
+    "chung2014empirical": {
+        "author": "Chung, Junyoung and Gulcehre, Caglar and Cho, Kyunghyun and Bengio, Yoshua",
+        "doi": "10.48550/arXiv.1412.3555",
+        "journal": "arXiv preprint arXiv:1412.3555",
+        "title": "Empirical Evaluation of Gated Recurrent Neural Networks on Sequence Modeling",
+        "type": "article",
+        "url": "https://doi.org/10.48550/arXiv.1412.3555",
+        "year": "2014"
+    },
+    "devlin2019bert": {
+        "author": "Devlin, Jacob and Chang, Ming-Wei and Lee, Kenton and Toutanova, Kristina",
+        "booktitle": "Proceedings of the 2019 Conference of the North American Chapter of the Association for Computational Linguistics: Human Language Technologies, Volume 1 (Long and Short Papers)",
+        "doi": "10.48550/arXiv.1810.04805",
+        "pages": "4171--4186",
+        "title": "BERT: Pre-training of Deep Bidirectional Transformers for Language Understanding",
+        "type": "inproceedings",
+        "url": "https://doi.org/10.48550/arXiv.1810.04805",
+        "year": "2019"
+    },
+    "khurana2022natural": {
+        "author": "Khurana, Diksha and Koli, Aditya and Khatter, Kiran and Singh, Sukhdev",
+        "doi": "10.1007/s11042-022-13428-4",
+        "journal": "Multimedia Tools and Applications",
+        "month": "Aug",
+        "number": "26",
+        "pages": "36581--36609",
+        "title": "Natural language processing: state of the art, current trends and challenges",
+        "type": "article",
+        "url": "https://doi.org/10.1007/s11042-022-13428-4",
+        "volume": "81",
+        "year": "2022"
+    },
+    "manning2014stanford": {
+        "author": "Manning, Christopher D and Surdeanu, Mihai and Bauer, John and Finkel, Jenny and Bethard, Steven J and McClosky, David",
+        "booktitle": "Proceedings of the 52nd Annual Meeting of the Association for Computational Linguistics: System Demonstrations",
+        "doi": "10.3115/v1/P14-5010",
+        "pages": "55--60",
+        "title": "The Stanford CoreNLP natural language processing toolkit",
+        "type": "inproceedings",
+        "url": "https://doi.org/10.3115/v1/P14-5010",
+        "year": "2014"
+    },
+    "mikolov2013distributed": {
+        "author": "Mikolov, Tomas and Chen, Kai and Corrado, Greg and Dean, Jeffrey",
+        "booktitle": "Advances in Neural Information Processing Systems",
+        "doi": "10.48550/arXiv.1310.4546",
+        "pages": "3111--3119",
+        "title": "Distributed Representations of Words and Phrases and their Compositionality",
+        "type": "inproceedings",
+        "url": "https://doi.org/10.48550/arXiv.1310.4546",
+        "year": "2013"
+    },
+    "papineni2002bleu": {
+        "author": "Papineni, Kishore and Roukos, Salim and Ward, Todd and Zhu, Wei-Jing",
+        "booktitle": "Proceedings of the 40th Annual Meeting of the Association for Computational Linguistics",
+        "doi": "10.3115/1073083.1073135",
+        "pages": "311--318",
+        "title": "BLEU: A Method for Automatic Evaluation of Machine Translation",
+        "type": "inproceedings",
+        "url": "https://doi.org/10.3115/1073083.1073135",
+        "year": "2002"
+    },
+    "pennington2014glove": {
+        "author": "Pennington, Jeffrey and Socher, Richard and Manning, Christopher",
+        "booktitle": "Proceedings of the 2014 Conference on Empirical Methods in Natural Language Processing (EMNLP)",
+        "doi": "10.3115/v1/D14-1162",
+        "pages": "1532--1543",
+        "title": "GloVe: Global Vectors for Word Representation",
+        "type": "inproceedings",
+        "url": "https://doi.org/10.3115/v1/D14-1162",
+        "year": "2014"
+    },
+    "vaswani2017attention": {
+        "author": "Vaswani, Ashish and Shazeer, Noam and Parmar, Niki and Uszkoreit, Jakob and Jones, Llion and Gomez, Aidan N and Kaiser, \u0141ukasz and Polosukhin, Illia",
+        "booktitle": "Advances in Neural Information Processing Systems",
+        "doi": "10.48550/arXiv.1706.03762",
+        "pages": "5998--6008",
+        "title": "Attention Is All You Need",
+        "type": "inproceedings",
+        "url": "https://doi.org/10.48550/arXiv.1706.03762",
+        "year": "2017"
+    }
+}});
